377b6bba87172ac1b9870727be04112ff34f540c
Shyam3097 <97452238+Shyam3097@users.noreply.github.com>
2023-01-17 15:16:47 +0530

    fixing an error

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const route = require("./routers/route");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 require("dotenv").config();
 
 app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 3000;
 const db_url = process.env.DATABASE_URL;
